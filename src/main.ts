ad2f9114ef608eb87089d2680a23b8d0afd93335
Lysander-ayudate <Lysander.Paniagua@ayudatelta.com>
2024-04-01 17:16:30 -0600

    Socket llamando assemblyAI

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.setGlobalPrefix('api/');
+  app.enableCors();
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
+
   await app.listen(3001);
+
   const logger = new Logger();
   logger.log(`Application started on port ${await app.getUrl()}`);
 }
--- src/transcription/transcription.gateway.ts
+++ src/transcription/transcription.gateway.ts
@@ -1,21 +1,49 @@
-import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import * as fs from 'fs';
+import axios from 'axios';
+import { AssemblyAI } from 'assemblyai';
 
-@WebSocketGateway()
+@WebSocketGateway(3010, { namespace: 'transcription', cors: { origin: '*' } }) // * http://localhost:3010/transcription
 export class TranscriptionGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  @SubscribeMessage('videoData')
+  async handleVideo(@ConnectedSocket() client: Socket, @MessageBody() data: any) {
+    console.log(`Received video data from client: ${client.id}`);
+    
+    fs.writeFile('video.wav', data, (err) => {
+      if (err) {
+        console.error('Error al guardar el archivo de video:', err);
+      } else {
+        console.log('Archivo de video guardado exitosamente');
+      }
+    });
+
+    // const clientAssembly = new AssemblyAI({
+    //   apiKey: 'f07b771a2bad42d386e0755e6b6be933',
+    // });
+
+    // try {
+    //   const transcript = await clientAssembly.transcripts.transcribe({
+    //     audio: data,
+    //     language_code: 'es'
+    //   })
+    //   console.log({ transcript });
+    //   console.log(transcript.text)
+
+    // } catch (error) {
+    //   console.log(error);
+    // }
+
   }
 
-  handleConnection(client: Socket, ...args: any[]) {
+  handleConnection(client: Socket) {
     console.log('Client connected: ' + client.id);
   }
 
-  handleDisconnect(client: any) {
+  handleDisconnect(client: Socket) {
     console.log('Client disconnected: ' + client.id);
   }
 }
