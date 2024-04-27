e1ac9081a7ca60f350ab3723762233ee2625669e
Lysander-ayudate <Lysander.Paniagua@ayudatelta.com>
2024-04-27 16:21:36 -0600

    Admitir archivos grandes de hasta 200 MB - Diferenciar habladores

--- src/transcription/transcription.gateway.ts
+++ src/transcription/transcription.gateway.ts
@@ -4,43 +4,65 @@ import * as fs from 'fs';
 import axios from 'axios';
 import { AssemblyAI } from 'assemblyai';
 
-@WebSocketGateway(3010, { namespace: 'transcription', cors: { origin: '*' } }) // * http://localhost:3010/transcription
+@WebSocketGateway(3010, {
+  namespace: 'transcription',
+  cors: { origin: '*' },
+  maxHttpBufferSize: 200 * 1024 * 1024
+}) // * http://localhost:3010/transcription
 export class TranscriptionGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage('videoData')
-  async handleVideo(@ConnectedSocket() client: Socket, @MessageBody() data: any) {
-    console.log(`Received video data from client: ${client.id}`);
-    
-    fs.writeFile('video.wav', data, (err) => {
-      if (err) {
-        console.error('Error al guardar el archivo de video:', err);
-      } else {
-        console.log('Archivo de video guardado exitosamente');
-      }
+  @SubscribeMessage('recorderData')
+  async handleAudio(@ConnectedSocket() client: Socket, @MessageBody() data: any) {
+    const fileName = 'recorder.wav';
+
+    try {
+      fs.writeFileSync(fileName, data)
+      console.log('Archivo de grabacion guardado exitosamente');
+    } catch (error) {
+      console.error('Error al guardar el archivo de grabacion:', error);
+    }
+
+    const clientAssembly = new AssemblyAI({
+      apiKey: 'f07b771a2bad42d386e0755e6b6be933',
     });
 
-    // const clientAssembly = new AssemblyAI({
-    //   apiKey: 'f07b771a2bad42d386e0755e6b6be933',
-    // });
+    try {
+      const transcript = await clientAssembly.transcripts.create({
+        audio_url: fileName,
+        speaker_labels: true,
+        language_code: 'es'
+      })
 
-    // try {
-    //   const transcript = await clientAssembly.transcripts.transcribe({
-    //     audio: data,
-    //     language_code: 'es'
-    //   })
-    //   console.log({ transcript });
-    //   console.log(transcript.text)
+      let text = '';
 
-    // } catch (error) {
-    //   console.log(error);
-    // }
+      for (let utterance of transcript.utterances) {
+        const line = `Speaker ${utterance.speaker}: ${utterance.text}\n`;
+        text += line;
+      }
+      
+      console.log(text);
+      fs.writeFileSync('text-speakers.txt', text);
+      console.log('Archivo de texto guardado exitosamente');
+      client.emit(text);
+
+    } catch (error) {
+      console.log(error);
+    }
 
   }
 
   handleConnection(client: Socket) {
     console.log('Client connected: ' + client.id);
+
+    client.on('disconnect', (reason) => {
+      console.log('Client reason:', reason);
+    });
+
+    client.on('error', (error) => {
+      console.error('Socket error:', error);
+    });
   }
 
   handleDisconnect(client: Socket) {
