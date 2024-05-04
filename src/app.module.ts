4f0b67abf4ddd1af11fb5e10c5c666677217f214
Lysander-ayudate <Lysander.Paniagua@ayudatelta.com>
2024-05-04 10:16:10 -0600

    Subir audio y luego transcribirlo en otra funcion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TranscriptionModule } from './transcription/transcription.module';
 import { MyaudioModule } from './myaudio/myaudio.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TranscriptionModule, MyaudioModule],
+  imports: [
+    TranscriptionModule,
+    MyaudioModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env'
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/transcription/transcription.gateway.ts
+++ src/transcription/transcription.gateway.ts
@@ -1,57 +1,27 @@
-import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
 import * as fs from 'fs';
 import axios from 'axios';
+import { Server, Socket } from 'socket.io';
 import { AssemblyAI } from 'assemblyai';
 
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer
+} from '@nestjs/websockets';
+
 @WebSocketGateway(3010, {
-  namespace: 'transcription',
   cors: { origin: '*' },
   maxHttpBufferSize: 200 * 1024 * 1024
-}) // * http://localhost:3010/transcription
+}) // * http://localhost:3010
 export class TranscriptionGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  private fileName = 'recorder.wav';
+
   @WebSocketServer()
   server: Server;
-
-  @SubscribeMessage('recorderData')
-  async handleAudio(@ConnectedSocket() client: Socket, @MessageBody() data: any) {
-    const fileName = 'recorder.wav';
-
-    try {
-      fs.writeFileSync(fileName, data)
-      console.log('Archivo de grabacion guardado exitosamente');
-    } catch (error) {
-      console.error('Error al guardar el archivo de grabacion:', error);
-    }
-
-    const clientAssembly = new AssemblyAI({
-      apiKey: 'f07b771a2bad42d386e0755e6b6be933',
-    });
-
-    try {
-      const transcript = await clientAssembly.transcripts.create({
-        audio_url: fileName,
-        speaker_labels: true,
-        language_code: 'es'
-      })
-
-      let text = '';
-
-      for (let utterance of transcript.utterances) {
-        const line = `Speaker ${utterance.speaker}: ${utterance.text}\n`;
-        text += line;
-      }
-      
-      console.log(text);
-      fs.writeFileSync('text-speakers.txt', text);
-      console.log('Archivo de texto guardado exitosamente');
-      client.emit(text);
-
-    } catch (error) {
-      console.log(error);
-    }
-
-  }
 
   handleConnection(client: Socket) {
     console.log('Client connected: ' + client.id);
@@ -67,5 +37,47 @@ export class TranscriptionGateway implements OnGatewayConnection, OnGatewayDisco
 
   handleDisconnect(client: Socket) {
     console.log('Client disconnected: ' + client.id);
+  }
+
+  @SubscribeMessage('transcript')
+  async transcript(@ConnectedSocket() client: Socket) {
+    const clientAssembly = new AssemblyAI({
+      apiKey: process.env.ASSEMBLY_KEY,
+    });
+
+    try {
+      console.log('Transcripting...');
+      const transcript = await clientAssembly.transcripts.transcribe({
+        audio: this.fileName,
+        speaker_labels: true,
+        language_code: 'es'
+      })
+
+      let text = '';
+
+      for (let utterance of transcript.utterances) {
+        const line = `Speaker ${utterance.speaker}: ${utterance.text}\n`;
+        text += line;
+      }
+
+      fs.writeFileSync('text-speakers.txt', text);
+      console.log('Archivo de texto guardado exitosamente');
+      client.emit('audioTranscripted', { text });
+
+    } catch (error) {
+      console.error(error);
+      client.emit('transcriptError', { message: 'Error al guardar el archivo.', error: error.message });
+    }
+  }
+
+
+  @SubscribeMessage('uploadFileToServer')
+  async uploadFileToServer(@ConnectedSocket() client: Socket, @MessageBody() data: any) {
+    try {
+      fs.writeFileSync(this.fileName, data)
+    } catch (error) {
+      console.error(error);
+      client.emit('uploadFileError', { message: 'Error al guardar el archivo.', error: error.message });
+    }
   }
 }
