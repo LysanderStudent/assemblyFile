9d65be55ad98a47c25f2dbdfa52a217f3ff22b30
Lysander-ayudate <Lysander.Paniagua@ayudatelta.com>
2024-05-24 16:12:49 -0600

    cors para todos

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/');
   app.enableCors({
-    origin: 'https://send-record.vercel.app',
+    origin: '*',
     methods: ['POST', 'PUT', 'DELETE', 'GET']
   });
   app.use(json({ limit: '50mb' }));
